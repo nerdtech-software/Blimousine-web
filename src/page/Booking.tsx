f91b026e32cfc20f10af90860e580d6b2d55e6e0
shivam-1205 <shivamjsingh1205@gmail.com>
2024-11-18 15:58:06 +0530

    date and time

--- src/page/Booking.tsx
+++ src/page/Booking.tsx
@@ -96,6 +96,42 @@ const Booking = () => {
       </div>
 
 
+      <div className="-mx-3 flex flex-wrap">
+        <div className="w-full px-3 sm:w-1/2">
+          <div className="mb-5">
+            <label
+             
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Date
+            </label>
+            <input
+              type="date"
+              name="date"
+              id="date"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+        </div>
+        <div className="w-full px-3 sm:w-1/2">
+          <div className="mb-5">
+            <label
+
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Time
+            </label>
+            <input
+              type="time"
+              name="time"
+              id="time"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+        </div>
+      </div>
+
+
 
 
       <div className=" mb-5">
