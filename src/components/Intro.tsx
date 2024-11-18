b00178d7e885ee582da6d9b3f379f4533ffbd797
shivam-1205 <shivamjsingh1205@gmail.com>
2024-11-18 12:33:14 +0530

    some update

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -46,7 +46,7 @@ const Intro = () => {
                 >
                     We Always <br /> Care Your Trip
                 </h1>
-                <button className="text-white bg-Fire hover:bg-Fire-500 focus:ring-4 focus:ring-Fire-300 font-medium rounded-lg text-sm px-10 py-2.5 mt-4 dark:bg-Fire-600 dark:hover:bg-red-900 focus:outline-none dark:focus:ring-red-800">
+                <button className="text-white bg-Fire hover:bg-Fire-500 focus:ring-4 focus:ring-Fire-300 font-medium rounded-lg text-sm px-10 py-3 mt-4 dark:bg-Fire-600 dark:hover:bg-red-900 focus:outline-none dark:focus:ring-red-800">
                         Call Me
                     </button>
             </div>
